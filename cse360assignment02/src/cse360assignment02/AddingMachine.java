518ad96ba91e34e707682f1d4256c496beb924b8
KyleTheEpic <kjatkins@asu.edu>
2020-10-07 19:19:19 -0700

    First Commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,61 @@
+/**
+ * An AddingMachine object is meant to add and subtract values from some total (starting at 0), and keep track of those operations in a string.
+ * @author Kyle J. Atkinson
+ * @version 1.0
+ * @since 7-10-2020
+ * 
+ */
+package cse360assignment02;
+
+/**
+ * 
+ */
+public class AddingMachine {
+
+	private int total;
+	/**
+	 * Constructor instantiates AddingMachine object, giving the total int 0.
+	 */
+	public AddingMachine() {
+		// TODO Auto-generated constructor stub
+		total = 0;
+	}
+	/**
+	 * getTotal() returns the total variable int.
+	 * @param ()
+	 * @return total
+	 */
+	public int getTotal () {
+	    return 0;
+	  }
+	  /**
+	   * add(int value) adds value to total.
+	   * @return void
+	   * @param (int value)
+	   */
+	  public void add (int value) {
+	  }
+	  /**
+	   * subtract(int value) subtracts value from total.
+	   * @return int
+	   * @param value
+	   */
+	  public void subtract (int value) {
+	  }
+	  /**
+	   * toString() method returns the String where all the operations on total have been recorded.
+	   * @return String
+	   * @param ()
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  /**
+	   * clear() method makes the record String empty and puts the total back to 0
+	   * @return void
+	   * @param ()
+	   */
+	  public void clear() {
+	  }
+
+}
